856acb40fb12954e7fc2e173ba7bd4511a7baa90
Arpaesis <arpaesis@gmail.com>
2018-12-31 01:46:52 -0500

    Tidied up main.js, added true environment checking.

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 const electron = require('electron');
 const url = require('url');
 const path = require('path');
+const isDev = require('electron-is-dev');
 
-const {app, BrowserWindow, Menu, globalShortcut} = electron;
-
-process.env.NODE_ENV = 'development';
+const { app, BrowserWindow, Menu, globalShortcut } = electron;
 
 let mainWindow;
 
+//TODO: Make this path constant a global value, accessible from anywhere in the project.
 //init some directories
 const zerra_path = 'C:/Users/' + process.env.username + '/AppData/Roaming/.zerra/Launcher/';
 
@@ -17,7 +17,7 @@ mkdirp(zerra_path + "/Instances/", {});
 mkdirp(zerra_path + "/Settings/", {});
 
 // Listen for app to be ready
-app.on('ready', function() {
+app.on('ready', function () {
 
     // Create new window
     mainWindow = new BrowserWindow({
@@ -28,17 +28,16 @@ app.on('ready', function() {
     });
 
     mainWindow.setMenu(null);
-    //TODO: make this work only in dev mode.
-    //mainWindow.openDevTools();
+
     // Load html into window
     mainWindow.loadURL(url.format({
         pathname: path.join(__dirname, 'main.html'),
-        protocol:'file:',
+        protocol: 'file:',
         slashes: true
     }));
 
-    //Register keybindings
-    if(process.env.NODE_ENV == 'development') {
+    //Register keybindings. These keybindings are only to be used inside the development environment.
+    if (isDev) {
         globalShortcut.register('CommandOrControl+R', () => {
             const window = require('electron').BrowserWindow;
             window.getFocusedWindow().reload();
@@ -54,59 +53,9 @@ app.on('ready', function() {
             window.getFocusedWindow().openDevTools();
         });
     }
-    
 
     //Quit app when closed
-    mainWindow.on('closed', function() {
+    mainWindow.on('closed', function () {
         app.quit();
     });
-
-    /*
-    // Build menu from template
-    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
-
-    // Insert menu
-    Menu.setApplicationMenu(mainMenu);
-    */
 });
-
-/*
-// Create menu template
-const mainMenuTemplate = [
-    {
-        label: 'File',
-        submenu: [
-            {
-                label: 'Quit',
-                accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
-                click() {
-                    app.quit();
-                }
-            }
-        ]
-    }
-];
-
-if(process.platform == 'darwin') {
-    mainMenuTemplate.unshift({});
-}
-
-// Add developer tools item if not in production
-if(process.env.NODE_ENV !== 'production') {
-    mainMenuTemplate.push({
-        label: 'Developer Tools',
-        submenu: [
-            {
-                label: 'Toggle DevTools',
-                accelerator: process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
-                click(item, focusedWindow) {
-                    focusedWindow.toggleDevTools();
-                }
-            },
-            {
-                role: 'reload'
-            }
-        ]
-    });
-}
-*/
